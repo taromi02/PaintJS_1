3b2bb13413e50a85947c1fe95b426258f79f0f3d
taromi02 <100741494+taromi02@users.noreply.github.com>
2022-03-28 23:07:00 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
 const canvas = document.getElementById("jscanvas")
+const ctx = canvas.getContext("2d")
+
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.strokeStyle = "#2c2c2c"
+ctx.LineWidth = 2.5;
 
 Let painting = false; 
 
-function stoppainting(){
+function stopPainting(){
     painting = false;
+}
+
+function startPainting(){
+    painting = true;
 }
 
 function onMouseMove(event) {
     const x = event.offsetX;
-    const y = event.pffsetY;
+    const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.LineTo(x, y);
+        ctx.stroke()
+    }
 }
 
 function onMouseDown(event) {
     painting = true;
 }
 
-function onMouseUp(event){
-    stoppainting();
-}
-
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
-    canvas.addEventListener("mouseleave", stoppainting);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseLeave", stoppainting);
 }
